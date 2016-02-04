ea166e0baf473e39b8e78acf1128b7aef7c387a6
sago35 <sago35@gmail.com>
2016-02-04 19:57:08 +0900

    テストがFailするパターンを登録

--- lib/Max.pm
+++ lib/Max.pm
@@ -1,12 +1,20 @@
 package Max;
 
 sub max {
-    my ($in1, $in2) = @_;
+    my ($in1, $in2, $in3) = @_;
 
     if ($in1 < $in2) {
-        return $in2;
+        if ($in2 < $in3) {
+            return $in3;
+        } else {
+            return $in2;
+        }
     } else {
-        return $in1;
+        if ($in1 < $in3) {
+            return $in3;
+        } else {
+            return $in1;
+        }
     }
 }
 
--- t/max.t
+++ t/max.t
@@ -4,6 +4,11 @@ use Max;
 ok(Max::max(3,5) == 5, "max(3,5) == 5"); # ok 1
 is Max::max(-2,4), 4;
 
+is Max::max(-2,-4), -2;
+
+is Max::max(1,2,3), 3;
+is Max::max(5), 5;
+
 
 
 done_testing;
