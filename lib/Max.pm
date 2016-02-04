afdf0cd683f8eb9c2fbeb1ff3a3e09071bf20300
sago35 <sago35@gmail.com>
2016-02-04 20:02:28 +0900

    配列での処理に変更

--- lib/Max.pm
+++ lib/Max.pm
@@ -1,29 +1,13 @@
 package Max;
 
 sub max {
-    my ($in1, $in2, $in3) = @_;
-
-    if ($in1 < $in2) {
-        if (defined $in3) {
-            if ($in2 < $in3) {
-                return $in3;
-            } else {
-                return $in2;
-            }
-        } else {
-            return $in2;
-        }
-    } else {
-        if (defined $in3) {
-            if ($in1 < $in3) {
-                return $in3;
-            } else {
-                return $in1;
-            }
-        } esle {
-            return $in1;
+    my $max = shift @_;
+    foreach my $value (@_) {
+        if ($max < $value) {
+            $max = $value;
         }
     }
+    return $max;
 }
 
 1;
